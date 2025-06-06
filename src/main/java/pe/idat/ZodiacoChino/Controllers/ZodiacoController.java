d2c14ab181b928494793d02378560b0180242501
inoricode28 <inori237@gmail.com>
2025-06-06 00:29:44 -0500

    inicio

---
+++ src/main/java/pe/idat/ZodiacoChino/Controllers/ZodiacoController.java
@@ -0,0 +1,5 @@
+package pe.idat.ZodiacoChino.Controllers;
+
+public class ZodiacoController {
+
+}
---
+++ src/test/java/pe/idat/ZodiacoChino/ZodiacoChinoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.idat.ZodiacoChino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZodiacoChinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
